59aa50dff1bf7a70454b7724f0fd01b1d2aa2e6d
Andrey Sidorenko <andrey1828@tut.by>
2021-05-16 01:25:55 +0300

    Fix gitlab properties builder

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.flassie.imlab"
-version = "0.1.1"
+version = "0.1.3"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/io/flassie/imlab/ImlabPluginExtension.kt
+++ src/main/kotlin/io/flassie/imlab/ImlabPluginExtension.kt
@@ -4,6 +4,9 @@ open class ImlabPluginExtension {
     var gitlabPublishing: GitlabPublishing? = null
 
     fun gitlabPublishing(builder: GitlabPublishing.() -> Unit) {
-        GitlabPublishing().builder()
+        val newVersion = GitlabPublishing()
+        newVersion.builder()
+
+        gitlabPublishing = newVersion
     }
 }
